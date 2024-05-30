ef12f2a3f4165f151720535383b6148a72c6dc12
sae <alexxxkish@gmail.com>
2024-05-30 07:21:20 +0500

    забыл засунуть крутилку загрузки в гит

---
+++ src/components/load.tsx
@@ -0,0 +1,12 @@
+import '../assets/loader.css';
+
+
+const Loader: React.FC = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loader;
